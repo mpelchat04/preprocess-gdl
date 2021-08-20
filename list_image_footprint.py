eb48cfd1f080abcf58b177f16a34a38d3d81c1db
Mathieu <mathieu.turgeon-pelchat@canada.ca>
2021-08-20 10:53:01 -0400

    image to footprint

--- list_image_footprint.py
+++ list_image_footprint.py
@@ -29,11 +29,13 @@ def get_footprint(tif_path):
 def main(root_folder):
     lst_img = [Path(name) for name in glob.glob(str(root_folder) + "/*.tif")]
     lst_info = []
+    print("listing geometries...", end="")
     for tif_path in lst_img:
         geom = get_footprint(tif_path=tif_path)
         tuile = str(Path(tif_path).stem)
         lst_info.append({'geom': geom, 'tuile': tuile})
 
+    print("done")
     data_schema = {
         'geometry': 'MultiPolygon',
         'properties': OrderedDict([
@@ -41,8 +43,9 @@ def main(root_folder):
         ])
     }
 
+    print(f"writing list to gpkg {str(root_folder.parent)}_list_tile.gpkg", end="")
     gpkg_path = root_folder / Path(str(root_folder.parent) + "_list_tile.gpkg")
-    with fiona.open(gpkg_path, 'w', layer=str(root_folder.parent), driver='GPKG', schema=data_schema) as c:
+    with fiona.open(gpkg_path, 'w', layer="table1", driver='GPKG', schema=data_schema) as c:
         for elem in lst_info:
             pol_to_write = {
                 'geometry': json.loads(elem['geom']),
@@ -52,6 +55,7 @@ def main(root_folder):
 
             }
             c.write(pol_to_write)
+    print("done")
 
 
 if __name__ == '__main__':
